decd739a426030d3f6c088fcf6007232bf433304
ANGIEGIS97 <angiedeconejito@gmail.com>
2024-07-12 12:28:52 -0500

    mejoras proyecto

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -1,58 +1,29 @@
 <template>
   <div class="bg-gray-100 min-h-screen py-8">
     <div class="container mx-auto px-4">
+      <!-- Título -->
+      <h1 class="text-3xl font-bold text-center text-blue-600 mb-8">
+        Calculadora de Consumo Eléctrico y Paneles Solares
+      </h1>
+
       <!-- Ingresar datos de electrodomésticos -->
       <div class="bg-white shadow-md rounded-lg p-6 mb-8">
         <h2 class="text-xl font-semibold mb-4 text-gray-700">
           Ingrese los datos de sus electrodomésticos:
         </h2>
+
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <!-- Iteración sobre cada electrodoméstico -->
-          <div
+          <ElectrodomesticoItem
             v-for="(electrodomestico, index) in electrodomesticos"
             :key="index"
-            class="bg-gray-50 p-4 rounded-md shadow"
-          >
-            <h3 class="text-lg font-medium text-gray-800 mb-2">
-              {{ electrodomestico.nombre }}
-            </h3>
-            <div class="space-y-3">
-              <!-- Input para la potencia del electrodoméstico -->
-              <div>
-                <label
-                  :for="`watts-${index}`"
-                  class="block text-sm font-medium text-gray-700"
-                  >Potencia (watts)</label
-                >
-                <input
-                  v-model.number="electrodomestico.watts"
-                  type="number"
-                  :id="`watts-${index}`"
-                  class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                  :placeholder="`Watts de ${electrodomestico.nombre}`"
-                />
-              </div>
-              <!-- Input para las horas de uso diario -->
-              <div>
-                <label
-                  :for="`hours-${index}`"
-                  class="block text-sm font-medium text-gray-700"
-                  >Horas de uso diario</label
-                >
-                <input
-                  v-model.number="electrodomestico.hoursPerDay"
-                  type="number"
-                  :id="`hours-${index}`"
-                  class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-                  placeholder="Horas por día"
-                />
-              </div>
-            </div>
-          </div>
+            :electrodomestico="electrodomestico"
+            :index="index"
+            @update="updateElectrodomestico"
+          />
         </div>
 
         <!-- Componente de información del panel solar -->
-        <SolarPanelInfo @update:panel-info="updatePanelInfo" />
+        <InfoPanelSolar @update:panel-info="updatePanelInfo" />
 
         <!-- Botón para iniciar el cálculo -->
         <button
@@ -64,7 +35,7 @@
       </div>
 
       <!-- Componente de resultados -->
-      <ResultadosPanel
+      <Resultados
         :mostrarResultados="mostrarResultados"
         :consumoMensualTotal="consumoMensualTotal"
         :panelesSolaresRequeridos="panelesSolaresRequeridos"
@@ -78,22 +49,25 @@
 </template>
 
 <script>
-import SolarPanelInfo from "./SolarPanelInfo.vue";
-import ResultadosPanel from "./ResultadosPanel.vue";
+import InfoPanelSolar from "./InfoPanelSolar.vue";
+import Resultados from "./Resultados.vue";
+import ElectrodomesticoItem from "./ElectrodomesticoItem.vue";
 
 export default {
   components: {
-    SolarPanelInfo,
-    ResultadosPanel,
+    InfoPanelSolar,
+    Resultados,
+    ElectrodomesticoItem,
   },
   data() {
     return {
       electrodomesticos: [
-        { nombre: "Televisor", watts: 0, hoursPerDay: 0 },
-        { nombre: "Licuadora", watts: 0, hoursPerDay: 0 },
-        { nombre: "Bombilla", watts: 0, hoursPerDay: 0 },
-        { nombre: "Lavadora", watts: 0, hoursPerDay: 0 },
-        { nombre: "Computador", watts: 0, hoursPerDay: 0 },
+        { nombre: "Televisor", watts: 175, horasPorDia: 0, cantidad: 0 },
+        { nombre: "Licuadora", watts: 300, horasPorDia: 0, cantidad: 0 },
+        { nombre: "Bombilla", watts: 20, horasPorDia: 0, cantidad: 0 },
+        { nombre: "Lavadora", watts: 350, horasPorDia: 0, cantidad: 0 },
+        { nombre: "Computador", watts: 200, horasPorDia: 0, cantidad: 0 },
+        { nombre: "Brilladora", watts: 500, horasPorDia: 0, cantidad: 0 },
       ],
       solarPanelOutput: 250,
       horasSolDia: 5,
@@ -112,12 +86,24 @@ export default {
       this.eficienciaPanel = info.eficienciaPanel;
       this.horasSolDia = info.horasSolDia;
     },
+    updateElectrodomestico(electrodomestico) {
+      const index = this.electrodomesticos.findIndex(
+        (e) => e.nombre === electrodomestico.nombre
+      );
+      if (index !== -1) {
+        this.electrodomesticos[index] = electrodomestico;
+      }
+    },
     calcular() {
       this.consumoMensualTotal = this.electrodomesticos.reduce(
         (total, electrodomestico) => {
           return (
             total +
-            (electrodomestico.watts * electrodomestico.hoursPerDay * 30) / 1000
+            (electrodomestico.watts *
+              electrodomestico.horasPorDia *
+              30 *
+              electrodomestico.cantidad) /
+              1000
           );
         },
         0
---
+++ src/components/Resultados.vue
@@ -0,0 +1,165 @@
+<template>
+  <div v-if="mostrarResultados" class="bg-white shadow-md rounded-lg p-6">
+    <h2 class="text-2xl font-bold text-gray-800 mb-4 sm:text-left text-center">
+      Resultados
+    </h2>
+    <table class="w-full border-collapse">
+      <tbody>
+        <tr class="border-b bg-green-50">
+          <td class="py-2 font-medium">Consumo mensual total:</td>
+          <td class="py-2">
+            <span class="text-green-600 font-bold">
+              {{ consumoMensualTotal.toFixed(2) }} kWh
+            </span>
+            <span class="text-gray-600">
+              (${{ (consumoMensualTotal * 867.8).toFixed(2) }} pesos)
+            </span>
+          </td>
+        </tr>
+        <tr class="border-b bg-green-50">
+          <td class="py-2 font-medium">Consumo diario promedio:</td>
+          <td class="py-2">
+            <span class="text-green-600 font-bold">
+              {{ consumoDiarioPromedio.toFixed(2) }} kWh
+            </span>
+            <span class="text-gray-600">
+              (${{ (consumoDiarioPromedio * 867.8).toFixed(2) }} pesos)
+            </span>
+          </td>
+        </tr>
+        <tr class="border-b bg-green-50">
+          <td class="py-2 font-medium">Consumo anual total:</td>
+          <td class="py-2">
+            <span class="text-green-600 font-bold">
+              {{ consumoAnualTotal.toFixed(2) }} kWh
+            </span>
+            <span class="text-gray-600">
+              (${{ (consumoAnualTotal * 867.8).toFixed(2) }} pesos)
+            </span>
+          </td>
+        </tr>
+        <tr class="border-b bg-purple-50">
+          <td class="py-2 font-medium">Paneles solares necesarios:</td>
+          <td class="py-2">
+            <span class="text-purple-600 font-bold">
+              {{ panelesSolaresRequeridos }}
+            </span>
+          </td>
+        </tr>
+        <tr class="border-b bg-blue-50">
+          <td class="py-2 font-medium">Potencia del panel:</td>
+          <td class="py-2">
+            <span class="text-blue-600 font-bold">
+              {{ solarPanelOutput }} watts
+            </span>
+          </td>
+        </tr>
+        <tr class="border-b bg-blue-50">
+          <td class="py-2 font-medium">Eficiencia del panel:</td>
+          <td class="py-2">
+            <span class="text-blue-600 font-bold">
+              {{ eficienciaPanel.toFixed(1) }}%
+            </span>
+          </td>
+        </tr>
+        <tr class="border-b bg-blue-50">
+          <td class="py-2 font-medium">Tipo de panel:</td>
+          <td class="py-2">
+            <span class="text-blue-600 font-bold">{{ tipoPanelDisplay }}</span>
+          </td>
+        </tr>
+        <tr class="border-b bg-blue-50">
+          <td class="py-2 font-medium">Energía generada por año:</td>
+          <td class="py-2">
+            <span class="text-blue-600 font-bold">
+              {{ generacionEnergiaAnual.toFixed(2) }} kWh
+            </span>
+          </td>
+        </tr>
+        <tr class="border-b bg-yellow-50">
+          <td class="py-2 font-medium flex">
+            Ahorro estimado (25 años):
+            <span
+              ref="tooltip1"
+              class="ml-2 text-white bg-gray-400 rounded-full w-4 h-4 flex items-center justify-center cursor-pointer"
+              >?</span
+            >
+          </td>
+          <td class="py-2">
+            <span class="text-yellow-600 font-bold">
+              ${{ ahorroEstimado.toLocaleString() }} pesos
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+
+export default {
+  props: {
+    mostrarResultados: Boolean,
+    consumoMensualTotal: Number,
+    panelesSolaresRequeridos: Number,
+    solarPanelOutput: Number,
+    eficienciaPanel: Number,
+    tipoPanel: String,
+    generacionEnergiaAnual: Number,
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initTooltip();
+    });
+  },
+  updated() {
+    this.initTooltip();
+  },
+  methods: {
+    initTooltip() {
+      if (this.$refs.tooltip1) {
+        tippy(this.$refs.tooltip1, {
+          content:
+            "El calculo del ahorro considera *Costo anual de electricidad. *Energia generada anualmente por el sistema solar. *Inflacion anual estimada del 4% en el costo de la electricidad. *El ahorro se acumula año tras año considerando la Inflación. *25 años vida util panel solar",
+          placement: "top",
+          arrow: true,
+        });
+      }
+    },
+  },
+  computed: {
+    tipoPanelDisplay() {
+      const types = {
+        monocristalino: "Monocristalino",
+        polycristalino: "Policristalino",
+        peliculaDelgada: "Película delgada",
+      };
+      return types[this.tipoPanel];
+    },
+    consumoDiarioPromedio() {
+      return this.consumoMensualTotal / 30;
+    },
+    consumoAnualTotal() {
+      return this.consumoMensualTotal * 12;
+    },
+    ahorroEstimado() {
+      const costoElectricidadAnual = this.consumoAnualTotal * 867.8;
+      const ahorroAnual = Math.min(
+        costoElectricidadAnual,
+        this.generacionEnergiaAnual * 867.8
+      );
+      const inflacionAnual = 0.04; // 4% de inflación anual
+      let ahorroTotal = 0;
+
+      for (let año = 1; año <= 25; año++) {
+        ahorroTotal += ahorroAnual * Math.pow(1 + inflacionAnual, año - 1);
+      }
+
+      return Math.round(ahorroTotal);
+    },
+  },
+};
+</script>
